9eafd8e9b7bd6a14df58e3db6e92d50afa453c53
admin <admin@kajalab.cz>
2026-06-13 21:00:29 +0200

    commit 3dtisk

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
+import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   server: {
     host: true,   // naslouchá na 0.0.0.0 — dostupné i ze sítě
     port: 5173,
-  }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        tisk3d: resolve(__dirname, '3dtisk/index.html'),
+      },
+    },
+  },
 })
